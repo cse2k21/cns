1904c8fb84090dba5e31e92a3518fdbfef312c2c
cse2k21 <96973807+cse2k21@users.noreply.github.com>
2023-01-01 17:10:39 +0530

    Update railfence.c

--- railfence.c
+++ railfence.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-#include<string.h>
-void main(){
-char a[25] = "Meet me in toga Party";
-int i, j=0, k=0,l=0;
-char b[20],c[20];
-for (i=0;i<25;i++)
+#include <stdio.h>
+#include <string.h>
+void main()
 {
-if(a[i]!=' ')
-{
-if(j%2==0)
-{
-b[k++]=a[i];
-j++;
-}
-else
-{
-c[l++]=a[i];
-j++;
-}
-}
-}
-strcat(b,c);
-printf("cipher text:%s",b);
+    char a[25] = "Meet me in toga Party";
+    int i, j = 0, k = 0, l = 0;
+    char b[20], c[20];
+    for (i = 0; i < strlen(a); i++)
+    {
+        if (a[i] != ' ')
+        {
+            if (j % 2 == 0)
+            {
+                b[k++] = a[i];
+                j++;
+            }
+            else
+            {
+                c[l++] = a[i];
+                j++;
+            }
+        }
+    }
+    strcat(b, c);
+    printf("cipher text:%s", b);
 }
