9697f748e13110c74223e0c5809b003d5c51958c
cse2k21 <96973807+cse2k21@users.noreply.github.com>
2023-01-01 14:31:25 +0530

    Update ceasar.c

--- ceasar.c
+++ ceasar.c
@@ -1,25 +1,24 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
 void main()
 {
-char input[30];
-int key;
-printf("enter plain text:");
-scanf("%s",input);
-printf("enter the key:");
-scanf("%d",&key);
-for(int i=0;input[i]!='\0';i++)
-{
-	if(isupper(input[i]))
-	{
-	input[i]=(input[i]+key-65)%26+65;
-	}
-	else
-	{
-	input[i]=(input[i]+key-97)%26+97;
-	}
+    char input[30];
+    int key;
+    printf("Enter Plain text : ");
+    scanf("%s", input);
+    printf("Enter the key : ");
+    scanf("%d", &key);
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if (isupper(input[i]))
+        {
+            input[i] = ((input[i] + key - 65) % 26) + 65;
+        }
+        else
+        {
+            input[i] = ((input[i] + key - 97) % 26) + 97;
+        }
+    }
+    printf("Cipher text is : %s", input);
 }
-printf("cipher text is:%s",input);
-}
-
