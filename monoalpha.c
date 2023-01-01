a7e4d164e4fa7c9d4186863e80d27386830ba0ce
cse2k21 <96973807+cse2k21@users.noreply.github.com>
2023-01-01 16:40:36 +0530

    Update monoalpha.c

--- monoalpha.c
+++ monoalpha.c
@@ -1,35 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 void main()
 {
-char input[26];
-char pt[26];
-printf("enter key:");
-for(int i=0;i<26;i++)
-{
-scanf("%c",&input[i]);
-
-	for(int j=0;j<i;j++)
-	{	
-	if(input[j]==input[i])
-	{
-	printf("error:already entered the key");
-	exit(0);
-	}
-	}
+    char input[26], pt[26];
+    printf("Enter key : ");
+    for (int i = 0; i < 26; i++)
+    {
+        scanf("%c", &input[i]);
+        for (int j = 0; j < i; j++)
+        {
+            if (input[i] == input[j])
+            {
+                printf("error : already entered key");
+                exit(0);
+            }
+        }
+    }
+    printf("Enter plain text :");
+    scanf("%s", pt);
+    char out[26];
+    for (int i = 0; pt[i] != '\0'; i++)
+    {
+        if (isupper(pt[i]))
+            out[i] = input[pt[i] - 65];
+        else
+            out[i] = input[pt[i] - 97];
+    }
+    printf("Cipher text is : %s", out);
 }
-printf("enter plain text:");
-scanf("%s",pt);
-char out[26];
-for(int i=0;i<strlen(pt);i++)
-{
-	if(isupper(pt[i]))
-	out[i]=input[pt[i]-65];
-	else
-	out[i]=input[pt[i]-97];
-}
-printf("cipher text is: %s",out);
-}
-
