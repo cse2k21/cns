cdf287893de98fb901a4eedb02023b5801790a97
cse2k21 <96973807+cse2k21@users.noreply.github.com>
2023-01-02 09:47:45 +0530

    Update AES.java

--- AES.java
+++ AES.java
@@ -1,23 +1,23 @@
-import javax.swing.JOptionPane;
 import javax.crypto.*;
 import javax.crypto.spec.*;
-import java.io.*;
-public class AES 
+import java.util.*;
+public class AES
 {
-public static void main(String[] args) throws Exception
-{ 
-KeyGenerator kgen = KeyGenerator.getInstance("AES");
-Cipher cipher = Cipher.getInstance("AES");
-SecretKey skey = kgen.generateKey();
-byte[] raw = skey.getEncoded();
-SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-String inputText = JOptionPane.showInputDialog("Input your message:");
-byte[] encrypted = cipher.doFinal(inputText.getBytes());
-cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-byte[] decrypted = cipher.doFinal(encrypted);
-JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "\nEncrypted text: " + new String(encrypted)+ "\n" + 
-"\nDecrypted text: " +new String(decrypted));
-System.exit(0);
-}
+    public static void main(String[] args) throws Exception
+    {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKey skey = kgen.generateKey();
+        byte[] raw = skey.getEncoded();
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your message:");
+        String inputText = sc.next();
+        byte[] encrypted = cipher.doFinal(inputText.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        System.out.println("\nEncrypted text: " + new String(encrypted)+ "\n" + "\nDecrypted text: " +new String(decrypted));
+        System.exit(0);
+    }
 }
